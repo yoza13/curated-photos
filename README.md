9a189f648d0ec5d14389bc887531c5b3a0f18259
Yash Oza <yashmichael@gmail.com>
2022-07-18 11:52:28 -0400

    feat: 🎸 modify existing handler to handle more than one request

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 ### This lambda function uses the API provided by [Pexel](https://www.pexels.com/api/documentation/)
 
 ### In order to run this code base locally or in AWS Lambda, you would need a API key which can be created using above link
+
+### This repo exposes two endpoints using API Gateway as mentioned below
+
+- Get all Curated photos
+  This is a `GET` endpoint - `staging/curated-photos`
+  It returns all the curated photos from the Pexel Endpoint
+
+- Get photos based on search string
+  This is a `POST` endpoint - `staging/curated-photos`
+  It takes request body as below -
+  ```shell
+  {
+      "query": "Tiger"
+  }
+  ```
+  and return list of curated photos based on above string from Pexel
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const axios = require("axios").default;
 
-exports.handler = async (event) => {
-  const { data: curatedPhotos } = await axios({
-    method: "GET",
-    url: "https://api.pexels.com/v1/curated",
-    params: {
-      per_page: 80,
-    },
+async function callAPi({ method, url, params }) {
+  const { data } = await axios({
+    method,
+    url,
+    params,
     headers: {
       Authorization: process.env.API_KEY,
     },
   });
 
-  const finalCuratedPhotos = curatedPhotos.photos.map((photo) => {
+  return data;
+}
+
+exports.handler = async (event) => {
+  let result;
+  switch (event.http_method) {
+    case "GET":
+      result = await callAPi({
+        method: "GET",
+        url: "https://api.pexels.com/v1/curated",
+        params: {
+          per_page: 80,
+        },
+      });
+      break;
+    case "POST":
+      result = await callAPi({
+        method: "GET",
+        url: "https://api.pexels.com/v1/search",
+        params: {
+          query: event.body.query,
+        },
+      });
+      break;
+  }
+
+  const finalCuratedPhotos = result.photos.map((photo) => {
     return {
       id: photo.id,
       url: photo.url,
